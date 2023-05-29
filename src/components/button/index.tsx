8eccc178fffb782dfbc4b22d6dedb26d94b5e9c7
nerdchanii <nerdchanii@gmail.com>
2023-05-29 14:34:04 +0900

    fix(button): fix button props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 
 interface ButtonProps {
-  variant?: 'primary' | 'secondary' | 'tertiary' | 'text';
+  variant?: "primary" | "secondary" | "tertiary" | "text";
   outlined?: boolean;
   icon?: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ 
-  variant = 'primary', 
-  outlined = false, 
+const Button: React.FC<ButtonProps> = ({
+  variant = "primary",
+  outlined = false,
   icon,
-  children 
+  children,
 }) => {
   return (
-    <button className={`button ${variant} ${outlined ? 'outlined' : ''}`}>
+    <button className={`button ${variant} ${outlined ? "outlined" : ""}`}>
       {icon && icon}
       {children}
     </button>
@@ -21,4 +22,3 @@ const Button: React.FC<ButtonProps> = ({
 };
 
 export default Button;
-
