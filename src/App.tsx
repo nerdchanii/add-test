d34f1d8b701679193311070836bb38ea318d7c94
nerdchanii <nerdchanii@gmail.com>
2023-05-29 15:07:47 +0900

    fix(router): fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'  
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home'
-import Intro from './pages/Intro'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import Logout from './pages/Logout'
-import News from './pages/News'
-import Followers from './pages/Followers'
-import Explore from './pages/Explore'
-import Username from './pages/[username]'
+import { useState } from "react";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Intro from "./pages/Intro";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Logout from "./pages/Logout";
+import News from "./pages/News";
+import Followers from "./pages/Followers";
+import Explore from "./pages/Explore";
+import Username from "./pages/[username]";
+
+const BrowserRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" exact element={Home}>
+      <Route path="/intro" element={Intro} />
+      <Route path="/sign-up" element={SignUp} />
+      <Route path="/sign-in" element={SignIn} />
+      <Route path="/logout" element={Logout} />
+      <Route path="/news" element={News} />
+      <Route path="/followers" element={Followers} />
+      <Route path="/explore" element={Explore} />
+      <Route path="/:username" element={Username} />
+    </Route>
+  )
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/intro" component={Intro} />
-      <Route path="/sign-up" component={SignUp} />
-      <Route path="/sign-in" component={SignIn} />
-      <Route path="/logout" component={Logout} />
-      <Route path="/news" component={News} />
-      <Route path="/followers" component={Followers} />
-      <Route path="/explore" component={Explore} />
-      <Route path="/:username" component={Username} />
-    </BrowserRouter>
-  )
+  const [count, setCount] = useState(0);
+  return <RouterProvider router={BrowserRouter} />;
 }
 
-export default App
+export default App;
